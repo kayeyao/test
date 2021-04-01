e10df1b9351157b4cc79f379e094395bb08a600d
kayeyao <76432318+kayeyao@users.noreply.github.com>
2021-04-02 02:24:33 +0800

    Add files via upload

--- app_body.py
+++ app_body.py
@@ -36,12 +36,12 @@ def covid_stats(country,status,length):
     
 	st.write('Status of COVID-19 cases in the ' + country + ' as of ' + end_date.strftime("%b %d %Y") + ':')
 
-	covidstats = st.DataFrame({
+	covidstats = pd.DataFrame({
 			'Column': ['Total Cases', 'New Cases', 'Total Deaths', 'New Deaths', 'Total Vaccinations'], 
                  	'Data as of ' + end_date.strftime("%b %d %Y"): [f"{int(latest_status['total_cases'].iloc[0]):,d}",f"{int(latest_status['new_cases'].iloc[0]):,d}",f"{int(latest_status['total_deaths'].iloc[0]):,d}", f"{int(latest_status['new_deaths'].iloc[0]):,d}",f"{int(latest_status['total_vaccinations'].iloc[0]):,d}"]
-		})
+		}).set_index('Column', inplace=True)
     	
-	st.table(covidstats.set_index('Column', inplace=True))
+	st.table(covidstats)
 
 
 	if status == 'Daily New Cases':
