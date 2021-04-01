b23d03f8b2b5c8a04d2406ae4d361e2aa1321ffc
kayeyao <76432318+kayeyao@users.noreply.github.com>
2021-04-02 02:01:57 +0800

    Add files via upload

--- app_body.py
+++ app_body.py
@@ -38,9 +38,10 @@ def covid_stats(country,status,length):
 
 	stats = {
 			'': ['Total Cases', 'New Cases', 'Total Deaths', 'New Deaths', 'Total Vaccinations'], 
-                 	'Data as of' + end_date.strftime("%b %d %Y"): [int(latest_status['total_cases'].iloc[0]),int(latest_status['new_cases'].iloc[0]),int(latest_status['total_deaths'].iloc[0]), int(latest_status['new_deaths'].iloc[0]),int(latest_status['total_vaccinations'].iloc[0])]
+                 	'Data' + end_date.strftime("%b %d %Y"): [int(latest_status['total_cases'].iloc[0]),int(latest_status['new_cases'].iloc[0]),int(latest_status['total_deaths'].iloc[0]), int(latest_status['new_deaths'].iloc[0]),int(latest_status['total_vaccinations'].iloc[0])]
 		}
-    	st.table(stats)
+    	
+	st.table(stats)
 
 	st.write('Status of COVID-19 cases in the ' + country + ' as of ' + end_date.strftime("%b %d %Y") + ':')
 	st.write('\nTotal Cases: ' + str(int(latest_status['total_cases'].iloc[0])))
