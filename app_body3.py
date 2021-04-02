7eff15f431dca0aced63963bfe8ded9c6ad73a94
kayeyao <76432318+kayeyao@users.noreply.github.com>
2021-04-02 21:34:53 +0800

    Add files via upload

--- app_body3.py
+++ app_body3.py
@@ -378,7 +378,7 @@ def chatbot():
 	question = get_text()
 
 	if question.lower() != 'end':
-		response = chatbot.get_response(preprocessor(question))
+		response = chatbot.generate_response(preprocessor(question))
         		
 		st.text_area("Bot:", value = response, height=200, max_chars=None, key=None)
 		print('\nResponse: ' + str(response))
