bbeb4c77fe037af3d9028a6911cbb84e995c71f2
kayeyao <76432318+kayeyao@users.noreply.github.com>
2021-04-06 02:32:30 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown("""<style>.css-1aumxhk {background-color: #efede8; background-image:
 
 
 ## Side Bar Information
-image = Image.open('logo/eskwelabs.png')
+image = Image.open('TalkingVac logo 2.png')
 st.sidebar.image(image, caption='', use_column_width=True)
 
 
